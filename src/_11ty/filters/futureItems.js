a64e2a3b91453a1ae65b07d8efc6ab2a6ca3554f
Adam Duncan <mail@adamduncandesigns.com>
2020-11-25 17:15:11 +0000

    Set default param for collections, in case null

--- src/_11ty/filters/futureItems.js
+++ src/_11ty/filters/futureItems.js
@@ -1,14 +1,14 @@
 /**
- * 
+ *
  * Return items that are in the future compared to the build date.
  *
  * Time is reset 00:00 so check is based soley on yyyy/mm
  *
  * @param {Array} collection
- * 
+ *
  */
 
-module.exports = collection => {
+module.exports = (collection = []) => {
   const buildDate = new Date().setHours(0, 0, 0, 0);
 
   return collection.filter(item => {
