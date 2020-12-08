1e47f41cbb8b3251993522642cb1a2af60b976c2
Adam Duncan <mail@adamduncandesigns.com>
2020-12-08 11:09:23 +0000

    Initial pass at prebuild script to dupe missing files

---
+++ scripts/prebuild.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const cpy = require('cpy');
+const path = require('path');
+
+const site = require('../src/_data/site');
+const locales = require('../src/_data/locales');
+
+module.exports = (async () => {
+  console.log('Prebuilding...');
+
+  try {
+    const { defaultLocale } = site;
+    const supportingLocales = locales
+      .filter(({ code }) => code !== defaultLocale)
+      .map(({ code }) => code);
+
+    const filesToCopy = supportingLocales.map(code => {
+      return cpy(
+        // Any file types that should be duplicated to language sites.
+        // Important to copy .json and .js file types, as these will often
+        // contain the 11ty data required to set locales/collections.
+        '**/*.{html,md,njk,json,js,jpg,jpeg,png,svg}',
+        path.join(process.cwd(), `src/${code}`),
+        {
+          cwd: path.join(process.cwd(), `src/${defaultLocale}`),
+          overwrite: false,
+          parents: true,
+        }
+      );
+    });
+
+    const copiedFiles = await Promise.all(filesToCopy);
+    console.log(
+      `Copied ${copiedFiles.length} to ${supportingLocales.join(', ')}`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+})();
