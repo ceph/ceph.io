34846c14929801d9b6516118b562943a10b16779
Pete Robelou <probelou@gmail.com>
2021-01-22 11:00:48 +0000

    Remove click eventListener from toggle function, if statement to check secondary nav exists.

--- src/js/nav-secondary.js
+++ src/js/nav-secondary.js
@@ -2,17 +2,16 @@
 /*global document window*/
 
 (function() {
-
   const navigation = document.querySelector('[data-nav-secondary');
-  const toggle = navigation.querySelector('[data-nav-secondary-toggle]');
-  const navigation_sub = navigation.querySelector('[data-nav-secondary-sub]');
 
-  const media_query = window.matchMedia('(width < 64em)')
-
-  navigation.classList.add('js-nav-secondary');
-
-  function toggleNav() {
-    toggle.addEventListener('click', (e) => {
+  if (navigation) {
+    const toggle = navigation.querySelector('[data-nav-secondary-toggle]');
+    const navigation_sub = navigation.querySelector('[data-nav-secondary-sub]');
+    const media_query = window.matchMedia('(width < 64em)')
+  
+    navigation.classList.add('js-nav-secondary');
+  
+    function toggle_nav(e) {
       if (navigation.classList.contains("js-nav-secondary--opened")) {
         navigation.classList.remove('js-nav-secondary--opened');
         navigation_sub.setAttribute("aria-hidden", "true");
@@ -21,24 +20,25 @@
         navigation_sub.setAttribute("aria-hidden", "false");
       }
       e.preventDefault();
-    });
-  }
- 
-  function handle_size_change(e) {
-    if (e.matches) {
-      navigation_sub.setAttribute("aria-hidden", "true");
-      toggleNav();
     }
-    else {
-      navigation_sub.setAttribute("aria-hidden", "false");
-      navigation.classList.remove('js-nav-secondary--opened');
+   
+    function handle_size_change(e) {
+      if (e.matches) {
+        toggle.addEventListener('click', toggle_nav);
+        navigation_sub.setAttribute("aria-hidden", "true");
+      }
+      else {
+        toggle.removeEventListener('click', toggle_nav);
+        navigation_sub.setAttribute("aria-hidden", "false");
+        navigation.classList.remove('js-nav-secondary--opened');
+      }
     }
+    
+    // Register event listener
+    media_query.addEventListener('change', handle_size_change);
+    
+    // Initial check
+    handle_size_change(media_query);
   }
-  
-  // Register event listener
-  media_query.addEventListener('change', handle_size_change);
-  
-  // Initial check
-  handle_size_change(media_query);
 
 })();
