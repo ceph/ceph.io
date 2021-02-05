6253a73c832707b76c3e4e3b5f25dc406a70110d
Pete Robelou <probelou@gmail.com>
2021-02-05 18:20:40 +0000

    PR fixes.

--- src/js/off-canvas.js
+++ src/js/off-canvas.js
@@ -8,7 +8,7 @@
   const menu_open = header.querySelector('[data-menu-open]');
   const menu_close = header.querySelector('[data-menu-close]');
 
-  const media_query = window.matchMedia('(max-width: 64em)');
+  const media_query = window.matchMedia('(max-width: 63.9375em)');
 
   function toggleMenu() {
     menu_open.addEventListener('click', e => {
