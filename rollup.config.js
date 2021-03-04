01a038171eeb893b5976b439e23ddcf50eb6a40a
Pete Robelou <probelou@gmail.com>
2021-03-04 17:17:57 +0000

    npm plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,44 @@
+import { nodeResolve as resolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+const env = process.env.NODE_ENV;
+
 export default {
   input: 'src/js/main.js',
+
+  watch: {
+    include: 'src/js',
+    clearScreen: false,
+  },
+
+  plugins: [
+    getBabelOutputPlugin({
+      presets: ['@babel/preset-env'],
+    }),
+    resolve({
+      browser: true,
+    }),
+    commonjs(),
+  ],
+
   output: {
     file: 'dist/js/bundle.js',
     format: 'es',
+    plugins: [
+      terser({
+        ecma: 2018,
+        mangle: { toplevel: true },
+        compress: {
+          module: true,
+          toplevel: true,
+          unsafe_arrows: true,
+          drop_console: !env,
+          drop_debugger: !env,
+        },
+        output: { quote_style: 1 },
+      }),
+    ],
   },
 };
