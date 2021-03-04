343956ffc28e8f2e45c5a688f16dee7a92715bba
Pete Robelou <probelou@gmail.com>
2021-03-04 19:19:30 +0000

    Multiple bundles, optimise rollup config, coreJS to resolve regeneratorRuntime issue.

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,75 @@
 import { nodeResolve as resolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
 const env = process.env.NODE_ENV;
 
-export default {
-  input: 'src/js/main.js',
-
-  watch: {
-    include: 'src/js',
-    clearScreen: false,
-  },
-
-  plugins: [
-    getBabelOutputPlugin({
-      presets: ['@babel/preset-env'],
-    }),
-    resolve({
-      browser: true,
-    }),
-    commonjs(),
-  ],
-
-  output: {
-    file: 'dist/js/bundle.js',
-    format: 'es',
-    plugins: [
-      terser({
-        ecma: 2018,
-        mangle: { toplevel: true },
-        compress: {
-          module: true,
-          toplevel: true,
-          unsafe_arrows: true,
-          drop_console: !env,
-          drop_debugger: !env,
+const babelConfig = {
+  babelHelpers: 'bundled',
+  exclude: 'node_modules/**',
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: ['ie 11'],
         },
-        output: { quote_style: 1 },
-      }),
+        useBuiltIns: 'usage',
+        corejs: 3,
+      },
     ],
-  },
+  ],
 };
+
+const terserConfig = {
+  ecma: 2018,
+  mangle: { toplevel: true },
+  compress: {
+    module: true,
+    toplevel: true,
+    unsafe_arrows: true,
+    drop_console: !env,
+    drop_debugger: !env,
+  },
+  output: { quote_style: 1 },
+};
+
+export default [
+  // Main bundle used on every page
+  {
+    input: 'src/js/main.js',
+
+    plugins: [
+      commonjs(),
+      resolve({
+        browser: true,
+      }),
+      babel(babelConfig),
+    ],
+
+    output: {
+      file: 'dist/js/bundle-main.js',
+      format: 'iife',
+      plugins: [terser(terserConfig)],
+    },
+  },
+  // Search bundle used only for blog search
+  {
+    input: 'src/js/search.js',
+
+    plugins: [
+      commonjs(),
+      resolve({
+        browser: true,
+      }),
+      babel(babelConfig),
+    ],
+
+    output: {
+      file: 'dist/js/bundle-search.js',
+      format: 'iife',
+      plugins: [terser(terserConfig)],
+    },
+  },
+];
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
+import 'details-polyfill';
+import 'focus-visible';
 import { NavSecondary } from './nav-secondary';
 import { OffCanvas } from './off-canvas';
 import { StickyScroll } from './sticky-scroll';
-import 'details-polyfill';
-import 'focus-visible';
 
 NavSecondary.init();
 OffCanvas.init();
