9fe1665b725f75f982e9697a17ac68d802615f45
Pete Robelou <probelou@gmail.com>
2021-03-05 12:08:44 +0000

    Optimise rollup config for multiple bundles, module and nomodule bundles.

--- rollup.config.js
+++ rollup.config.js
@@ -3,73 +3,52 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
-const env = process.env.NODE_ENV;
+const srcDir = `./src/js/`;
+const distDir = 'dist/js/';
 
-const babelConfig = {
-  babelHelpers: 'bundled',
-  exclude: 'node_modules/**',
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          browsers: ['ie 11'],
+const pluginsES = () => [
+  resolve(),
+  commonjs(),
+  process.env.NODE_ENV === 'development' && terser(),
+];
+
+const pluginsES5 = () => [
+  resolve(),
+  commonjs(),
+  babel({
+    babelHelpers: 'bundled',
+    exclude: 'node_modules/**',
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            browsers: ['ie 11'],
+          },
+          useBuiltIns: 'usage',
+          corejs: 3,
         },
-        useBuiltIns: 'usage',
-        corejs: 3,
-      },
+      ],
     ],
-  ],
-};
+  }),
+  process.env.NODE_ENV === 'development' && terser(),
+];
 
-const terserConfig = {
-  ecma: 2018,
-  mangle: { toplevel: true },
-  compress: {
-    module: true,
-    toplevel: true,
-    unsafe_arrows: true,
-    drop_console: !env,
-    drop_debugger: !env,
-  },
-  output: { quote_style: 1 },
-};
+function setupBuild(plugins, src, dist) {
+  return {
+    input: srcDir + src,
+    output: {
+      file: distDir + dist,
+      format: 'iife',
+      sourcemap: process.env.NODE_ENV === 'development' ? false : 'inline',
+    },
+    plugins: plugins,
+  };
+}
 
 export default [
-  // Main bundle used on every page
-  {
-    input: 'src/js/main.js',
-
-    plugins: [
-      commonjs(),
-      resolve({
-        browser: true,
-      }),
-      babel(babelConfig),
-    ],
-
-    output: {
-      file: 'dist/js/bundle-main.js',
-      format: 'iife',
-      plugins: [terser(terserConfig)],
-    },
-  },
-  // Search bundle used only for blog search
-  {
-    input: 'src/js/search.js',
-
-    plugins: [
-      commonjs(),
-      resolve({
-        browser: true,
-      }),
-      babel(babelConfig),
-    ],
-
-    output: {
-      file: 'dist/js/bundle-search.js',
-      format: 'iife',
-      plugins: [terser(terserConfig)],
-    },
-  },
+  setupBuild(pluginsES(), 'main.js', 'bundle-main.mjs'),
+  setupBuild(pluginsES5(), 'main.js', 'bundle-main.js'),
+  setupBuild(pluginsES(), 'search.js', 'bundle-search.mjs'),
+  setupBuild(pluginsES5(), 'search.js', 'bundle-search.js'),
 ];
