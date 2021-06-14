890e4f6ff37cd0bd50f0cf3b3c2d1c8f7de7254a
Pete Robelou <probelou@gmail.com>
2021-06-14 15:54:18 +0100

    CSS optimisation
    - npm postcss-purgecss
    - configured to look at .html, .njk, .js
    - regex for defaultExtractor and safelist

--- postcss.config.js
+++ postcss.config.js
@@ -5,5 +5,12 @@ module.exports = ({ env }) => ({
     require('postcss-preset-env')({ stage: 1 }), // https://preset-env.cssdb.org/features#stage-1
     // Other plugins...
     env === 'production' ? require('cssnano')({ preset: 'default' }) : null, // How to use conditional plugins/options for prod
+    env === 'production'
+      ? require('@fullhuman/postcss-purgecss')({
+          content: ['./src/**/*.html', './src/**/*.njk', './src/**/*.js'],
+          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+          safelist: { deep: [/^js-/] },
+        })
+      : null,
   ],
 });
