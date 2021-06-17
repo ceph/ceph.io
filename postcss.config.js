107cd414588495c03f29b5ceda29aeba1daca6d1
Pete Robelou <probelou@gmail.com>
2021-06-17 17:00:45 +0100

    Minor tweaks to purgecss and cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -4,13 +4,11 @@ module.exports = ({ env }) => ({
     require('postcss-normalize'),
     require('postcss-preset-env')({ stage: 1 }), // https://preset-env.cssdb.org/features#stage-1
     // Other plugins...
-    env === 'production' ? require('cssnano')({ preset: 'default' }) : null, // How to use conditional plugins/options for prod
-    env === 'production'
-      ? require('@fullhuman/postcss-purgecss')({
-          content: ['./src/**/*.html', './src/**/*.njk', './src/**/*.js'],
-          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-          safelist: { deep: [/^js-/] },
-        })
-      : null,
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./src/**/*.html', './src/**/*.njk', './src/**/*.js'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+      safelist: { deep: [/^js-/] },
+    }),
+    env === 'production' ? require('cssnano')({ preset: 'default' }) : null,
   ],
 });
