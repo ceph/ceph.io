f296a7385cfa780b5e2480542f4ec018fe0fb519
Adam Duncan <mail@adamduncandesigns.com>
2021-06-18 12:27:45 +0100

    Utilise spaces for replacements for better word separation and compression

--- src/_11ty/filters/cleanSearchRaw.js
+++ src/_11ty/filters/cleanSearchRaw.js
@@ -5,10 +5,7 @@
  */
 
 module.exports = function (text) {
-  var content = new String(text);
-
-  // all lower case
-  var content = content.toLowerCase();
+  const content = new String(text).toLowerCase();
 
   // remove all html elements and new lines
   const html = /(&lt;.*?&gt;)|(<.*?>)/gi;
@@ -19,16 +16,17 @@ module.exports = function (text) {
   const deduped = [...new Set(words)];
   const result = deduped.join(' ');
 
-  const shortWords = /\b(the|a|an|and|am|you|I|to|if|of|off|me|my|on|in|it|is|at|as|we|do|be|has|but|was|so|no|not|or|up|for|ve|ll|re|s)\b/gi;
+  const shortWords =
+    /\b(the|a|an|and|am|you|I|to|if|of|off|me|my|on|in|it|is|at|as|we|do|be|has|but|was|so|no|not|or|up|for|ve|ll|re|s)\b/gi;
   const unicode = /[\u0000-\u001F\u007F-\u009F]/g;
   const punctuation = /[!“”‘’"#$%&'()*+,–./\\:;<=>?@[\]^_`{|}~¶]/g;
   const lineBreaks = /[\r\n]+/gm;
   const extraSpaces = /\s+/g;
 
   return result
-    .replace(shortWords, '')
-    .replace(unicode, '')
-    .replace(punctuation, '')
+    .replace(shortWords, ' ')
+    .replace(unicode, ' ')
+    .replace(punctuation, ' ')
     .replace(lineBreaks, ' ')
     .replace(extraSpaces, ' ');
 };
