79d08b867e9aba01d41d400c9dff20d03713baee
Adam Duncan <mail@adamduncandesigns.com>
2021-06-18 15:56:22 +0100

    Tidy cleanSearchRaw module particulars

--- src/_11ty/filters/cleanSearchRaw.js
+++ src/_11ty/filters/cleanSearchRaw.js
@@ -4,7 +4,7 @@
  * @param {String} text
  */
 
-module.exports = function (text) {
+module.exports = function (text = '') {
   const content = new String(text).toLowerCase();
 
   // remove all html elements and new lines
@@ -14,7 +14,7 @@ module.exports = function (text) {
   const shortWords =
     /\b(the|a|an|and|am|you|I|to|if|of|off|me|my|on|in|it|is|at|as|we|do|be|has|but|was|so|no|not|or|up|for|ve|ll|re|s)\b/gi;
   const unicode = /[\u0000-\u001F\u007F-\u009F]/g;
-  const punctuation = /[!“”‘’"#$%&'()*+,–./\\:;<=>?@[\]^_`{|}~¶]/g;
+  const punctuation = /[!“”‘’"#$%&'()*+,./\\:;<=>?@[\]^_`{|}~¶]/g;
   const lineBreaks = /[\r\n]+/gm;
   const extraSpaces = /\s+/g;
 
