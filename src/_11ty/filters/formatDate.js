1e2d1257e9126d910847e74514cc98cdb68cb99c
Adam Duncan <mail@adamduncandesigns.com>
2021-06-30 20:43:40 +0200

    Set hours manually

--- src/_11ty/filters/formatDate.js
+++ src/_11ty/filters/formatDate.js
@@ -2,14 +2,11 @@ const site = require('../../_data/site');
 const { defaultLocale } = site;
 
 module.exports = (date, locale = defaultLocale) => {
-  // Dates should be set to UTC to avoid off-by-one issues
-  // Docs: https://www.11ty.dev/docs/dates/#dates-off-by-one-day
-  const dateUTC = new Date(date).toUTCString();
-
   return new Intl.DateTimeFormat(locale, {
     year: 'numeric',
     month: 'short',
     day: 'numeric',
-    timezone: 'UTC',
-  }).format(new Date(dateUTC));
+    // Dates should be set to UTC to avoid off-by-one issues
+    // Docs: https://www.11ty.dev/docs/dates/#dates-off-by-one-day
+  }).format(new Date(date).setHours(0, 0, 0));
 };
