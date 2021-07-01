f11e5250f7206a924bc3fda9cb0280b6aa0ca26e
Adam Duncan <mail@adamduncandesigns.com>
2021-07-01 09:21:15 +0200

    Confirm dates correctly formatted, remove logs

--- src/_11ty/filters/formatDate.js
+++ src/_11ty/filters/formatDate.js
@@ -2,12 +2,11 @@ const site = require('../../_data/site');
 const { defaultLocale } = site;
 
 module.exports = (date, locale = defaultLocale) => {
-  const format = new Intl.DateTimeFormat(locale, {
+  return new Intl.DateTimeFormat(locale, {
     year: 'numeric',
     month: 'short',
     day: 'numeric',
     timeZone: 'UTC',
   }).format(new Date(new Date(date).toUTCString()));
-  console.log(date, format);
   return format;
 };
